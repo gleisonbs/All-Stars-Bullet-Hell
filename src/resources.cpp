b3548efa3622dc528b7327f0b748827edf855223
gleisonbs <>
2018-11-05 20:10:11 -0200

    removes std:: from resources file

--- src/resources.cpp
+++ src/resources.cpp
@@ -8,47 +8,46 @@
 #include "../include/resources.hpp"
 #include "../include/util.hpp"
 
-std::map<std::string, sf::Texture> Resources::Textures = {};
-std::map<std::string, sf::Font> Resources::Fonts = {};
+std::map<string, sf::Texture> Resources::Textures = {};
+std::map<string, sf::Font> Resources::Fonts = {};
 
-void Resources::addTexture(const std::string &name, const std::string &path) {
+using std::string;
+using std::vector;
+
+void Resources::addTexture(const string &name, const string &path) {
     sf::Texture tex;
     tex.loadFromFile(path);
     Resources::Textures[name] = tex;
 }
 
-void Resources::addFont(const std::string &name, const std::string &path) {
+void Resources::addFont(const string &name, const string &path) {
     sf::Font font;
     font.loadFromFile(path);
     Resources::Fonts[name] = font;
 }
 
-void Resources::listFolderFiles(const std::string &path, int degree, std::string fullQualifier) {
+void Resources::listFolderFiles(const string &path, int degree, string fullQualifier) {
 #ifdef __MING32__
 	std::cout << "WINDOWS DEF" << std::endl;
     HANDLE hFind;
     WIN32_FIND_DATA data;
 
-    // removes backslash at the end
+    vector<string> folderList;
 
-    //fullQualifier = fullQualifier.substr(0, fullQualifier.size()-1);
-
-    std::vector<std::string> folderList;
-
-    std::string searchPath = path + "*.*";
+    string searchPath = path + "*.*";
     hFind = FindFirstFile(searchPath.c_str(), &data);
     if (hFind != INVALID_HANDLE_VALUE) {
         do {
-            if (std::string(data.cFileName) == "." or std::string(data.cFileName) == "..") continue;
+            if (string(data.cFileName) == "." or string(data.cFileName) == "..") continue;
             if ((data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 16) {
-                folderList.push_back(std::string(data.cFileName));
+                folderList.push_back(string(data.cFileName));
             }
             else {
                 for (int i = 0; i < degree; i++) std::cout << "\t";
                 //addTexture(fullQualifier, )
-                std::string key = split(fullQualifier.substr(1) + "_" + data.cFileName, '.')[0];
-                std::string fileExtension = split(fullQualifier.substr(1) + "_" + data.cFileName, '.')[1];
-                std::string value = path + data.cFileName;
+                string key = split(fullQualifier.substr(1) + "_" + data.cFileName, '.')[0];
+                string fileExtension = split(fullQualifier.substr(1) + "_" + data.cFileName, '.')[1];
+                string value = path + data.cFileName;
                 std::cout << "key: " << key << " - val: " << value << std::endl;
                 if (fileExtension == "ttf")
                     addFont(key, value);
@@ -68,9 +67,9 @@ void Resources::listFolderFiles(const std::string &path, int degree, std::string
 	d = opendir(path.c_str());
 	if (d) {
 		while ((dir = readdir(d)) != NULL) {
-			std::string filename = dir->d_name;
+			string filename = dir->d_name;
 			if (filename == "." or filename == "..") continue;
-			std::string tabs = std::string(degree, ' ');
+			string tabs = string(degree, ' ');
 			if (dir->d_type == DT_DIR) {
 				std::cout << tabs << path + filename + "/" << std::endl;
 				listFolderFiles(path + filename + "/", degree+1, fullQualifier + "_" + filename);
@@ -84,19 +83,19 @@ void Resources::listFolderFiles(const std::string &path, int degree, std::string
 #endif
 }
 
-void Resources::scan(const std::string &rootPath) {
-    std::string fullQualifier = "";
+void Resources::scan(const string &rootPath) {
+    string fullQualifier = "";
     listFolderFiles(rootPath, 0, fullQualifier);
 }
 
-sf::Font& Resources::getFont(const std::string &fontKey) {
+sf::Font& Resources::getFont(const string &fontKey) {
     std::cout << fontKey << std::endl;
     if (Resources::Fonts.count(fontKey) == 0)
         std::cout << "KEY NOT IN FONTS" << std::endl;
     return Resources::Fonts[fontKey];
 }
 
-sf::Texture& Resources::getTexture(const std::string &textureKey) {
+sf::Texture& Resources::getTexture(const string &textureKey) {
     std::cout << textureKey << std::endl;
     if (Resources::Textures.count(textureKey) == 0)
         std::cout << "KEY NOT IN RESOURCES" << std::endl;
