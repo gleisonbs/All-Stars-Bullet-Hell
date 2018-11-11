4c8090e76b8ac5ef03fc14b7c4ee16cb5f6f474f
gleisonbs <>
2018-11-11 10:28:38 -0200

    Add compiling script

---
+++ comp.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "CXX = g++" > Makefile
+
+echo "EXE = allstars" >> Makefile
+
+echo "LIBS = -lsfml-graphics -lsfml-window -lsfml-system" >> Makefile
+
+echo "IDIR = include/" >> Makefile
+
+echo -e "FLAGS = -std=c++11 -I\$(IDIR)" >> Makefile
+
+echo -n "OBJS = " >> Makefile
+for file in src/*
+do
+	filename=${file##*/}
+	filename=${filename%.*}
+	echo -n "$filename.o " >> Makefile
+done
+
+echo -e "\n\nall: \$(EXE)\n" >> Makefile
+
+echo -e "\$(EXE): \$(OBJS)" >> Makefile
+echo -e "\t\$(CXX) \$(LIBS) $^ -o \$@\n" >> Makefile
+
+for file in src/*
+do
+	filename=${file##*/}
+	filename=${filename%.*}
+	echo "$filename.o: src/$filename.cpp" >> Makefile
+	echo -e "\t\$(CXX) \$(FLAGS) -c -o \$@ \$<\n" >> Makefile
+done
+
+echo -e "main.o: src/main.cpp\n" >> Makefile
+
+echo -e "clean:\n\trm *.o *.gch include/*.gch" >> Makefile
+
+
+clear
+for arg in "$@"; do
+	echo $arg
+	if [[ $arg == *"c"* ]]; then
+		make clean 2> /dev/null
+	fi
+
+	if [[ $arg == *"m"* ]]; then
+		make
+	fi
+done
+
